018b9c0d7b4b5edc2e099818b64e8e7f84123464
Georgia Barnett <georgia.barnett1108@gmail.com>
2017-10-07 14:43:36 +1300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+import LayerContainer from './components/layerContainer';
+
+class App extends Component {
+  componentDidMount() {
+    window.addEventListener("scroll", this.onScroll.bind(this));
+  }
+
+  onScroll() {
+    window.requestAnimationFrame(this.scrollHandler.bind(this));
+  }
+
+  scrollHandler() {
+    var scrollY = Math.max(window.pageYOffset, 0);
+    this.container.layer0.update(scrollY);
+    this.container.layer1.update(scrollY);
+    this.container.layer2.update(scrollY);
+    this.container.layer3.update(scrollY);
+    this.container.layer4.update(scrollY);
+    this.container.layer5.update(scrollY);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <LayerContainer ref={(r) => this.container = r}/>
+        <div className="content">
+          <h1>This is parallax.</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/layerContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+import Layer from './layer';
+import './parallax.css';
+
+class LayerContainer extends Component {
+    render() {
+        return (
+            <div className="parallax-container">
+                <Layer key={0} ref={ref => (this.layer0 = ref)} className='parallax-layer layer-0' speed={0.05} maxScroll={565} />
+                <Layer key={1} ref={ref => (this.layer1 = ref)} className='parallax-layer layer-1' speed={0.1} maxScroll={565} />
+                <Layer key={2} ref={ref => (this.layer2 = ref)} className='parallax-layer layer-2' speed={0.3} maxScroll={565} />
+                <Layer key={3} ref={ref => (this.layer3 = ref)} className='parallax-layer layer-3' speed={0.5} maxScroll={565} />
+                <Layer key={4} ref={ref => (this.layer4 = ref)} className='parallax-layer layer-4' speed={0.7} maxScroll={565} />
+                <Layer key={5} ref={ref => (this.layer5 = ref)} className='parallax-layer layer-5' speed={0.9} maxScroll={565} />
+            </div>
+        )
+    }
+}
+
+export default LayerContainer;
